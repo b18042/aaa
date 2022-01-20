37686b627c592e3181acebf698d5254bd1672eab
kznk7 <56796881+kznk7@users.noreply.github.com>
2022-01-20 16:17:41 +0900

    Computer.java

--- Computer.java
+++ Computer.java
@@ -4,8 +4,8 @@ public class Computer {
     public static int decidesComputerHand() {
         //課題3 Computerの手の決定とその表示
         //Randomメソッドを使って1~3の数値を返す
-        Random ra = new Random();
-        return ra.nextInt(3) + 1;
-        
+        Random random = new Random();
+        int rnd = random.nextInt(3)+1;
+        return rnd;
     }
 }
